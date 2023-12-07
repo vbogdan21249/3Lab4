0981d469ef38fb791481890ef0a168759d019a37
vbogdan21249 <101635004+vbogdan21249@users.noreply.github.com>
2023-12-07 14:26:25 +0200

    initial commit

---
+++ ConsoleApp/Class.cs
@@ -0,0 +1,48 @@
+﻿namespace console
+{
+    public class Node
+    {
+        public Node Next { get; set; }
+        public object Data;
+        public Node() { }
+        public Node(object data) { Data = data; }
+    }
+    public class StackEventArgs : EventArgs
+    {
+        public string EventMessage { get; }
+        public StackEventArgs(string eventMessage) { EventMessage = eventMessage; }
+    }
+
+    public class Stack : Node
+    {
+        Node? Top;
+        public delegate void StackClearingHandler(object sender, StackEventArgs e);
+        public event StackClearingHandler StackEvent;
+        public Stack() { }
+        public void Pop()
+        {
+            if (Top == null) return;
+            Node n = Top;
+            Top = Top.Next;
+            OnStackEvent(" Top element removed.");
+        }
+        public void Push(object i)
+        {
+            Node n = new Node(i);
+            n.Next = Top;
+            Top = n;
+            OnStackEvent(" Added the element to the top.");
+        }
+        public void Clear()
+        {
+            Top = null;
+            OnStackEvent(" Stack is empty now.");
+        }
+        private void OnStackEvent(string message)
+        {
+            StackEvent?.Invoke(this, new StackEventArgs(message));
+        }
+
+    }
+
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+namespace console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // normal method
+            CharAmountDelegate CharAmount = CharAmountMethod;
+            double NormalTest = CharAmount("uniusm douam");
+            Console.WriteLine(NormalTest + " identical symbols matching the starting symbol");
+
+            // anonymous
+            CharAmount = delegate (string input)
+            {
+                double counts = Regex.Matches(input, $"{input[0]}").Count;
+                return counts;
+            };
+            double AnonymousNest = CharAmount("uniusm douam");
+            Console.WriteLine(AnonymousNest + " identical symbols matching the starting symbol");
+
+            // lambda
+            CharAmount = (input) =>
+            {
+                double counts;
+                counts = Regex.Matches(input, $"{input[0]}").Count;
+                return counts;
+            };
+
+            double LambdaTest = CharAmount("uniusm douam");
+            Console.WriteLine(LambdaTest + " identical symbols matching the starting symbol");
+
+            Stack stack = new Stack();
+            stack.StackEvent += StackClearingHandler;
+
+            stack.Push(5);
+            stack.Push(2);
+            stack.Pop();
+            stack.Clear();
+
+        }
+
+        public delegate double CharAmountDelegate(string input);
+        public static double CharAmountMethod(string input)
+        {
+            double counts = Regex.Matches(input, $"{input[0]}").Count;
+            return counts;
+        }
+        public static void StackClearingHandler(object sender, StackEventArgs eventArgs)
+        {
+            Console.WriteLine($"Stack event:{eventArgs.EventMessage}");
+        }
+
+
+
+    }
+}
